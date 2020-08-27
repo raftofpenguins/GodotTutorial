231a34e93b39dc56a2f4d6c6e835ef2dfed4591b
Raft of Penguins <raftofpenguins@gmail.com>
2020-08-27 13:42:41 -0700

    Added player script and added WASD controls

--- Tutorial Project/Dodge/Player.gd
+++ Tutorial Project/Dodge/Player.gd
@@ -11,14 +11,25 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	var velocity = Vector2()
 	
 	# Check for input
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1	
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
 
-
-
-	# Move in given direction
-
-
-
-	# Play appropriate animation
-	pass
+	# Move in given direction & Play appropriate animation; clamp prevents leaving screen
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
