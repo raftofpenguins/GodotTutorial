72c4bb4f6d6a0ce30becc6f3968ee21032e6d5c4
Raft of Penguins <raftofpenguins@gmail.com>
2020-08-27 17:16:07 -0700

    Added music and death SFX

--- Tutorial Project/Dodge/Main.gd
+++ Tutorial Project/Dodge/Main.gd
@@ -38,6 +38,8 @@ func game_over():
 	$MobTimer.stop()
 	$HUD.show_game_over()
 	get_tree().call_group("mobs", "queue_free")
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -46,5 +48,7 @@ func new_game():
 
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+
+	$Music.play()
 
 	print("New game!")
