afbba2cb89c941b05a6fb639634f9a085e9a84b5
Raft of Penguins <raftofpenguins@gmail.com>
2020-08-27 17:04:39 -0700

    Finished main scene HUD integration

--- Tutorial Project/Dodge/Main.gd
+++ Tutorial Project/Dodge/Main.gd
@@ -10,6 +10,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_MobTimer_timeout():
 	# Choose a random location on Path2D.
@@ -35,9 +36,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
 	print("New game!")
